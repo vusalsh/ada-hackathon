17520ce9e4a13539cd917abebab715c75bc4b060
VusalSh <vusalshirinbayli@gmail.com>
2024-03-02 14:49:38 +0400

    try catch

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -16,54 +16,57 @@ export class ReportService {
     private readonly reportRepository: Repository<Report>,
   ) {}
   async create(user: User, file: Express.Multer.File) {
-    const existingReport: Report = await this.reportRepository.findOne({
-      where: { user: { id: user.id } },
-    });
-    if (existingReport) {
-      return existingReport;
-    }
-    const tempDir = await dir({
-      unsafeCleanup: true,
-    });
-    await fs.promises.writeFile(
-      `${tempDir.path}/${file.originalname}`,
-      file.buffer,
-    );
-
-    const proc = new ffmpeg(`${tempDir.path}/${file.originalname}`);
-    return await proc.then(async (video) => {
-      video.fnExtractFrameToJPG(`storage`, {
-        number: 1,
-        keep_pixel_aspect_ratio: true,
-        keep_aspect_ratio: true,
+    try {
+      const existingReport: Report = await this.reportRepository.findOne({
+        where: { user: { id: user.id } },
       });
-
-      const dotIndex = file.originalname.lastIndexOf('.');
-      const fileName = file.originalname.substring(0, dotIndex);
-
-      const filePath = `storage/${fileName}_1.jpg`;
-
-      if (fs.existsSync(filePath)) {
-        const buffer = fs.readFileSync(filePath);
-        const file_blob = new Blob([buffer]);
-
-        const formData = new FormData();
-        formData.append('image', file_blob);
-        const response = await axios.post(
-          `${process.env.MODEL_API_URL}/process_image`,
-          formData,
-        );
-        if (response.status === 200) {
-          console.log(response);
-          const reportData: IReport = response.data;
-          const report: Report = this.reportRepository.create(reportData);
-          report.user = user;
-          return await this.reportRepository.save(report);
-        } else {
-          console.log(response);
-        }
+      if (existingReport) {
+        return existingReport;
       }
-    });
+      const tempDir = await dir({
+        unsafeCleanup: true,
+      });
+      await fs.promises.writeFile(
+        `${tempDir.path}/${file.originalname}`,
+        file.buffer,
+      );
+
+      const proc = new ffmpeg(`${tempDir.path}/${file.originalname}`);
+      return await proc.then(async (video) => {
+        video.fnExtractFrameToJPG(`storage`, {
+          number: 1,
+          keep_pixel_aspect_ratio: true,
+          keep_aspect_ratio: true,
+        });
+
+        const dotIndex = file.originalname.lastIndexOf('.');
+        const fileName = file.originalname.substring(0, dotIndex);
+
+        const filePath = `storage/${fileName}_1.jpg`;
+
+        if (fs.existsSync(filePath)) {
+          const buffer = fs.readFileSync(filePath);
+          const file_blob = new Blob([buffer]);
+
+          const formData = new FormData();
+          formData.append('image', file_blob);
+          const response = await axios.post(
+            `${process.env.MODEL_API_URL}/process_image`,
+            formData,
+          );
+          if (response.status === 200) {
+            const reportData: IReport = response.data;
+            const report: Report = this.reportRepository.create(reportData);
+            report.user = user;
+            return await this.reportRepository.save(report);
+          } else {
+            console.log(response);
+          }
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async getImage(name: string) {
